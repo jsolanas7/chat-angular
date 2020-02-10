5ff0bdfcb0471014b154f6891f42ba23c93c5e80
Juan Andrés Solanas <jsolanas@mstech.la>
2020-02-10 11:36:58 -0300

    Fix url prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  url: 'https://chat-arjonero.herokuapp.com/'
+  url: 'https://chat-arjonero.herokuapp.com/chat/',
+  urlSocket: 'https://chat-arjonero.herokuapp.com',
+
 };
